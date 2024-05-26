cb6b8a23bcd021b05f263fefad18606003b5f409
Elijah Reck <kcerhajile@gmail.com>
2024-05-26 13:16:29 -0400

    Added PhoneBookContext for db

--- PhoneBook/Model.cs
+++ PhoneBook/Model.cs
@@ -1,21 +1,27 @@
-﻿namespace PhoneBook
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PhoneBook;
+
+public class PhoneBookContext : DbContext
 {
-    
-    public class Contact
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string PhoneNumber { get; set; }
-        public int CategoryId { get; set; }
-        public Category Category { get; set; }
-    }
+    public DbSet<Contact> Contacts { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+        optionsBuilder.UseSqlServer(); // 
+}
+public class Contact
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public int CategoryId { get; set; }
+    public Category Category { get; set; }
+}
 
-    public class Category
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public List<Contact> Contacts { get; } = new();
-    }
-
+public class Category
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public List<Contact> Contacts { get; } = new();
 }
